a1ac8f7646b8b998aaab3ba5e26fec32386aab08
AryanAAB <aryanbansal310@gmail.com>
2024-10-25 12:26:57 +0530

    Initial Commit

---
+++ src/main/java/org/example/FirebaseInitializer.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FirebaseInitializer
+{
+    public static void initializeFirebase()
+    {
+        try
+        {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+            System.out.println("Firebase initialized successfully!");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error initializing Firebase: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        initializeFirebase();
+    }
+}
