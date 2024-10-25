c0f1442046f1432a71da0112efc5ce86b64c7f07
AryanAAB <aryanbansal310@gmail.com>
2024-10-25 13:59:03 +0530

    Test

--- src/main/java/org/example/FirebaseInitializer.java
+++ src/main/java/org/example/FirebaseInitializer.java
@@ -1,35 +1,46 @@
 package org.example;
 
+import com.google.auth.oauth2.GoogleCredentials;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.auth.oauth2.GoogleCredentials;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class FirebaseInitializer
+public final class FirebaseInitializer
 {
-    private static void initializeFirebase()
-    {
-        try
-        {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            FirebaseApp.initializeApp(options);
-            System.out.println("Firebase initialized successfully!");
-        }
-        catch (IOException e)
-        {
-            System.out.println("Error initializing Firebase: " + e.getMessage());
-        }
-    }
-
-    public static void main(String[] args)
+    public FirebaseInitializer() throws IOException
     {
         initializeFirebase();
+    }
+
+    private void initializeFirebase() throws IOException
+    {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+
+    public void createUser(String email, String password) throws FirebaseAuthException
+    {
+        UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                .setEmail(email)
+                .setPassword(password);
+        FirebaseAuth.getInstance().createUser(request);
+    }
+
+    public static void main(String [] args) throws IOException, FirebaseAuthException
+    {
+        FirebaseInitializer initializer = new FirebaseInitializer();
+        initializer.createUser("hello@gamil.com", "hello12351!!!");
     }
 }
