2dbb33160ed18ce7747fb9b2960056a61c362e53
AryanAAB <aryanbansal310@gmail.com>
2024-10-30 09:34:03 +0530

    Fixed Server Side Code and added Client Side Code.

--- src/main/java/org/example/ServerFirebase.java
+++ src/main/java/org/example/ServerFirebase.java
@@ -1,13 +1,11 @@
 package org.example;
 
 import com.google.auth.oauth2.GoogleCredentials;
-
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 
 import java.io.FileInputStream;
 import java.io.IOException;
