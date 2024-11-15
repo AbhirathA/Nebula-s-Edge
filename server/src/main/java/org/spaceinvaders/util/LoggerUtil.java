49d0bd610f8e9e2ab4700691836aa91c7557d9e6
AryanAAB <aryanbansal310@gmail.com>
2024-11-15 18:39:51 +0530

    Added Logging Code for Server

--- server/src/main/java/org/spaceinvaders/util/LoggerUtil.java
+++ server/src/main/java/org/spaceinvaders/util/LoggerUtil.java
@@ -1,52 +1,26 @@
 package org.spaceinvaders.util;
 
-import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class LoggerUtil
-{
-    private static final Logger logger;
-    public static final String LOG_FILE = "../serverLog.log";
-
-    static
-    {
-        logger = Logger.getLogger(LoggerUtil.class.getName());
-
-        try
-        {
-            FileHandler filehandler = new FileHandler(LOG_FILE, true);
-            filehandler.setFormatter(new SimpleFormatter());
-            logger.addHandler(filehandler);
-            logger.setUseParentHandlers(false); //disables console logging
-        }
-        catch (IOException e)
-        {
-            logger.log(Level.SEVERE, "Failed to open log file", e);
-        }
-    }
+public class LoggerUtil {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerUtil.class);
 
     private LoggerUtil() {}
 
-    public static void logInfo(String message)
-    {
-        logger.log(Level.INFO, message);
+    public static void logInfo(String message) {
+        logger.info(message);
     }
 
-    public static void logWarning(String message)
-    {
-        logger.log(Level.WARNING, message);
+    public static void logWarning(String message) {
+        logger.warn(message);
     }
 
-    public static void logError(String message)
-    {
-        logger.log(Level.SEVERE, message);
+    public static void logError(String message) {
+        logger.error(message);
     }
 
-    public static void logException(String message, Exception e)
-    {
-        logger.log(Level.SEVERE, message, e);
+    public static void logException(String message, Exception e) {
+        logger.error(message, e);
     }
 }
