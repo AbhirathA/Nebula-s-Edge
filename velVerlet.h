1187c7db8b676a96e4399c9178155b460f4163db
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-11-27 04:50:52 +0530

    Update velVerlet.h
    Added AngleObj properties

--- velVerlet.h
+++ velVerlet.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "LinearObj.h"
+#include "AngleObj.h"
+
 class velVerlet : public LinearObj{
     public:
         velVerlet(int id, int x, int y, int vX, int vY, int accX, int accY, int innerRad, int outerRad, int mass) : LinearObj(id, x, y, vX, vY, accX, accY, innerRad, outerRad, mass) {}
@@ -8,9 +10,11 @@ class velVerlet : public LinearObj{
         virtual int getNextY(int t) override final;
 
         virtual bool checkCollision(LinearObj* obj);
+        virtual bool checkCollision(AngleObj* obj) { return obj->checkCollision(this); }
         virtual bool checkCollision(Obj* obj) { return obj->checkCollision(this); }
 
         virtual bool collisionCorrection(LinearObj* other);
+        virtual bool collisionCorrection(AngleObj* other) { return other->collisionCorrection(this); }
         virtual bool collisionCorrection(Obj* other) { return other->collisionCorrection(this); }
 
         virtual bool boundCorrection(int lft, int rt, int tp, int bt, int t);
