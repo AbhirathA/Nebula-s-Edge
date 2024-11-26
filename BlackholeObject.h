b867e151b75eae4174be6be2b8941fb60426f233
Dedeepya Avancha <deepya1235@gmail.com>
2024-11-26 21:07:17 +0530

    update blackholeobject

--- BlackholeObject.h
+++ BlackholeObject.h
@@ -5,54 +5,74 @@
 #include "FixedObj.h"
 #include <set>
 
-class Blackhole : public FixedObj
+class BlackholeObject : public FixedObj
 {
 private:
     int innerRadius;
     static constexpr int G = 10000;
     static constexpr int FLOATING_ERROR = 1;
+    static constexpr int GROWTH_FACTOR = 1; // fidn a good value maybe 0.0001
     std::set<FixedObj *> objects;
 
 public:
-    Blackhole(int id, int x, int y, int innerRad, int outerRad, int mass) : FixedObj(id, x, y, innerRad, outerRad, mass), innerRadius(innerRad) {};
+    BlackholeObject(int id, int x, int y, int innerRad, int outerRad, int mass) : FixedObj(id, x, y, innerRad, outerRad, mass), innerRadius(innerRad) {
+                                                                                  };
 
     void addObject(FixedObj *obj)
     {
         objects.insert(obj);
     }
+    void absorb(FixedObj *obj)
+    {
+        this->mass += obj->getMass();
+        this->outerRad += obj->getMass() * GROWTH_FACTOR;
+        this->updateBox();
+    }
+    void interactWith(FixedObj *obj)
+    {
+        if (dynamic_cast<BlackholeObject *>(obj))
+            return;
+
+        int dx = this->getX() - obj->getX();
+        int dy = this->getY() - obj->getY();
+        int distanceSquared = dx * dx + dy * dy;
+
+        if (distanceSquared < (this->innerRadius + obj->getInnerR()) * (this->innerRadius + obj->getInnerR()))
+        {
+            obj->selfDestruct();
+            this->absorb(obj);
+        }
+        else if (distanceSquared < (this->outerRad + obj->getOuterR()) * (this->outerRad + obj->getOuterR()))
+        {
+            applyGravitationalForce(obj, dx, dy, distanceSquared);
+        }
+    }
+    void applyGravitationalForce(FixedObj *obj, int dx, int dy, int distanceSquared)
+    {
+        double distance = std::sqrt(static_cast<double>(distanceSquared));
+        int force = static_cast<int>(G * this->getMass() * obj->getMass() / distanceSquared);
+
+        int accX = static_cast<int>(force * (dx / distance));
+        int accY = static_cast<int>(force * (dy / distance));
+
+        obj->updateV(accX, accY);
+    }
+
     void update()
     {
         std::vector<FixedObj *> toRemove;
         for (FixedObj *obj : objects)
         {
-            if (dynamic_cast<Blackhole *>(obj))
-            {
-                continue;
-            }
+            interactWith(obj);
 
-            int dx = this->getX() - obj->getX();
-            int dy = this->getY() - obj->getY();
-
-            int distanceSquared = dx * dx + dy * dy;
-            if (distanceSquared < (innerRad + obj->getInnerR()) * (innerRad + obj->getInnerR()) - FLOATING_ERROR)
-            {
-                obj->selfDestruct();
-                toRemove.push_back(obj);
-            }
-            else if (distanceSquared < (this->getOuterR() + obj->getOuterR()) * (this->getOuterR() + obj->getOuterR()) - FLOATING_ERROR)
-            {
-                double distance = std::sqrt(static_cast<double>(distanceSquared));
-                int force = static_cast<int>(G * this->getMass() * obj->getMass() / distanceSquared);
-
-                int accX = static_cast<int>(force * (dx / distance));
-                int accY = static_cast<int>(force * (dy / distance));
-
-                obj->updateAcc(accX, accY);
-            }
-            else
+            if (*obj->getStatus())
             {
                 toRemove.push_back(obj);
             }
+        }
+        for (FixedObj *obj : toRemove)
+        {
+            objects.erase(obj);
         }
     }
 };
