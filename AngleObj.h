e55f35605fb975d9fd22147d18bcccf210b2ea97
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-11-27 04:52:06 +0530

    Update AngleObj.h

--- AngleObj.h
+++ AngleObj.h
@@ -1,11 +1,30 @@
 #pragma once
 #include "Obj.h"
+#include "LinearObj.h"
 #include <cmath>
-#define SCALE 1000
+#define VALUE_SCALE 1000
+#define ANGLE_SCALE 10
 
-class AngleObj:Obj{
+class AngleObj: public Obj{
 	
+	private:
+		int mod(int a, int b) {
+			while (a > b) {
+				a -= b;
+			}
+			while (a < 0) {
+				a += b;
+			}
+			return a;
+		}
+
 	protected:
+		//Angle Computation Pre-requisites
+		static int SIN[3600];
+		static int COS[3600];
+		static double PI;
+		static bool initialized;
+
 		// Orientation
 		int angleScaled = 0;
 
@@ -21,10 +40,29 @@ class AngleObj:Obj{
 
 		AngleObj(int id, int x, int y, int v, int angle, int acc, int accX, int accY, int innerRad, int outerRad, int mass):Obj(id,x,y,innerRad,outerRad, mass) {
 			this->v = v;
-			this->angleScaled = angle;
+			this->angleScaled = (angle*ANGLE_SCALE)%(360*ANGLE_SCALE);
 			this->acc = acc;
 			this->accX = accX;
 			this->accY = accY;
+
+			std::cout << "Created AO: " << v << " " << angle << std::endl;
+			this->initializeTrig();
+		}
+
+		virtual int getvX() {
+			return this->v * COS[this->angleScaled];
+		}
+
+		virtual int getvY() {
+			return this->v * SIN[this->angleScaled];
+		}
+
+		virtual int getaccX() {
+			return this->acc * COS[this->angleScaled] + this->accX * VALUE_SCALE;
+		}
+
+		virtual int getaccY() {
+			return this->acc * SIN[this->angleScaled] + this->accY * VALUE_SCALE;
 		}
 
 		virtual int getV() {
@@ -34,6 +72,8 @@ class AngleObj:Obj{
 		virtual void updateV(int v) {
 			this->v = v;
 		}
+
+		virtual void updateV(int vX, int vY, int scale);
 
 		virtual int getXacc() {
 			return accX;
@@ -51,20 +91,24 @@ class AngleObj:Obj{
 			this->acc = a;
 		}
 
-		int getAngle() {
-			return this->angleScaled;
+		double getAngle() {
+			return ((double)this->angleScaled) / ANGLE_SCALE;
 		}
 
-		virtual bool checkCollision(Obj* obj) = 0;
-		virtual void updatePos(int t) = 0;
-		//void internalUpdate();
+		virtual bool checkCollision(Obj* obj);
+		virtual bool checkCollision(LinearObj* lo);
+		virtual bool checkCollision(AngleObj* ao);
 
-		virtual int getNextX(int t) = 0;
-		virtual int getNextY(int t) = 0;
+		virtual bool collisionCorrection(Obj* other);
+		virtual bool collisionCorrection(LinearObj* other);
+		virtual bool collisionCorrection(AngleObj* other);
 
-		virtual bool boundCorrection(int lft, int rt, int tp, int bt, int t) = 0;
-		virtual bool collisionCorection(Obj* other) = 0;
+		virtual void updatePos(int t);
+		virtual int getNextX(int t);
+		virtual int getNextY(int t);
+
+		virtual void initializeTrig();
+
+		virtual bool boundCorrection(int lft, int rt, int tp, int bt, int t);
 		virtual ~AngleObj() {};
-
 };
-
