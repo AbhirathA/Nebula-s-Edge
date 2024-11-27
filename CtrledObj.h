df3638acc842bcd842c46c3624cc5974d6521182
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-11-27 05:54:16 +0530

    Update CtrledObj.h

--- CtrledObj.h
+++ CtrledObj.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "AngleObj.h"
+#include "Lifetime.h"
+
 class CtrledObj : public AngleObj {
-	
+	Lifetime* thruster =  nullptr;
+	public:
+		CtrledObj():AngleObj(){
+		}
 };
 
