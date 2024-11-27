b64b388844ac9c07c121b5e3ad4b37d7f1a41e32
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-11-27 11:20:23 +0530

    Update CtrledObj.h

--- CtrledObj.h
+++ CtrledObj.h
@@ -9,6 +9,9 @@ class CtrledObj : public AngleObj {
 	Lifetime* moveCtrl = nullptr;
 	Lifetime* thrustBlock = nullptr;
 
+	bool isThrustable = true;
+
+
 
 	public:
 		CtrledObj(int id, int x, int y, int peakV, int driftV, int angle, int thrust, int thrustPersistance, int movePersistance, int coolDown, int accX, int accY, int innerRad, int outerRad, int mass):AngleObj(id, x, y, driftV, angle, 0, accX, accY, innerRad, outerRad, mass){
@@ -19,6 +22,29 @@ class CtrledObj : public AngleObj {
 
 			this->peakV = peakV;
 			this->thrust = thrust;
+		}
+
+		void reverseThrust(){
+			thrustReverse.start();
+			this->acc = (-1)*thrust;
+		}
+
+		void blockThrust(){
+			thrustBlocker.start();
+			this->isThrustable = false;
+			this->acc = 0;
+		}
+
+		void freeThrust(){
+			this->isThrustable = true;	
+		}
+
+		void startThrust(){
+			if(isThrustable){
+				this->acc = thrust;
+				thrustCtrl.start();
+				this->isThrustable = false;
+			}
 		}
 
 
