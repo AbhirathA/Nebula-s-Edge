a7b59b58be3cf0c399059b62a3719beb23e53e2f
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-11-27 12:06:53 +0530

    Add files via upload

---
+++ UserObj.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "CtrledObj.h"
+class UserObj : public CtrledObj {
+	
+	
+	public:
+		UserObj(int id, int x, int y, int peakV, int driftV, int angle, int thrust, int thrustPersistance, int movePersistance, int coolDown, int accX, int accY, int innerRad, int outerRad, int mass, int health) :CtrledObj(id, x, y, peakV, driftV, angle, thrust, thrustPersistance, movePersistance, coolDown, accX, accY, innerRad, outerRad, mass) {
+
+		}
+
+		virtual ~UserObj() {};
+};
+
