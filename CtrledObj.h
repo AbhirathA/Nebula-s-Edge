bdc648cd8cd62f94d60d12302596c9e5797cff74
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-11-27 10:40:53 +0530

    Update CtrledObj.h

--- CtrledObj.h
+++ CtrledObj.h
@@ -6,6 +6,8 @@ class CtrledObj : public AngleObj {
 	int thrust = 0;
 	Lifetime* thrustCtrl = nullptr;
 	Lifetime* moveCtrl = nullptr;
+	Lifetime* thrustBlock = nullptr;
+
 
 	public:
 		CtrledObj(int id, int x, int y, int peakV, int driftV, int angle, int thrust, int thrustPersistance, int movePersistance int accX, int accY, int innerRad, int outerRad, int mass):AngleObj(id, x, y, driftV, angle, 0, accX, accY, innerRad, outerRad, mass){
