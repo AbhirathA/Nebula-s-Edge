10862cb7651b508df326e05789be074640ac24b3
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2024-11-27 09:43:50 +0530

    Update CtrledObj.h

--- CtrledObj.h
+++ CtrledObj.h
@@ -2,5 +2,5 @@
 #include "AngleObj.h"
 class CtrledObj : public AngleObj {
 	public:
-		CtrledObj(int id, int x, int y, int peakV, int driftV int angle, int acc, int accX, int accY, int innerRad, int outerRad, int mass):AngleObj(id, x, y, v, angle, acc, accX, accY, innerRad, outerRad, mass)
+		CtrledObj(int id, int x, int y, int peakV, int driftV int angle, int thrust, int accX, int accY, int innerRad, int outerRad, int mass):AngleObj(id, x, y, driftV, angle, 0, accX, accY, innerRad, outerRad, mass)
 };
